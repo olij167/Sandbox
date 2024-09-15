90efdac48c51cb0160ac7c848af84189e329f9b2
Oli <1024958@student.sae.edu.au>
2024-09-15 12:05:14 +1000

    improve levitation ball, add roads

--- Assets/Character Controllers/Vehicle/FloatingVehicle.cs
+++ Assets/Character Controllers/Vehicle/FloatingVehicle.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class FloatingVehicle : Interactable
 {
     public float speed = 10f;
+    public float jumpHeight = 10f;
+    public bool isJumping;
+    public bool isFalling;
+    public bool isGrounded;
+
+    [SerializeField] private float currentGravScale = 1.0f;
+
     //public float rotationSpeed = 50f;
 
     public bool beingDriven;
@@ -18,7 +25,7 @@ public class FloatingVehicle : Interactable
     public float verticalHoverDistance = 0.25f;
     public float frequency = 1f;
 
-
+    public LayerMask playerLayer;
     private void Awake()
     {
         player = FindObjectOfType<PlayerController>();
@@ -29,7 +36,7 @@ public class FloatingVehicle : Interactable
     {
         RaycastHit hit;
 
-        if (Physics.Raycast(transform.position, -transform.up, out hit, Mathf.Infinity))
+        if (Physics.Raycast(transform.position, -transform.up, out hit, Mathf.Infinity, ~playerLayer))
         {
             Vector3 pos = hit.point;
 
@@ -46,13 +53,60 @@ public class FloatingVehicle : Interactable
                 //move = false;
 
                 moveDirection = (player.orientation.forward * Input.GetAxisRaw("Vertical")) + (player.orientation.right * Input.GetAxisRaw("Horizontal"));
-                moveDirection = moveDirection.normalized * speed * Time.deltaTime;
+                moveDirection = moveDirection.normalized * speed;
                 //moveDirection.y = floatDistance.y;
                 //moveDirection.y += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * verticalHoverDistance;
 
 
+                if (Input.GetButtonDown("Jump") && isGrounded)
+                {
+                    isJumping = true;
+                    isFalling = false;
+                    isGrounded = false;
+
+                    //moveDirection.y = jumpHeight;
+                }
+
+                if (isJumping)
+                {
+
+                    if (transform.position.y - floatDistance.y < jumpHeight)
+                    {
+                        moveDirection.y += jumpHeight;
+
+                    }
+                    else  if (transform.position.y - floatDistance.y > jumpHeight) // when they reach the jump height
+                    {
+                        moveDirection.y = Mathf.Lerp(moveDirection.y, 0, Time.deltaTime);
+
+                        if (moveDirection.y <= 0f)
+                        {
+                            isJumping = false;
+                            isFalling = true;
+                        }
+                    }
+
+                    transform.position += moveDirection * Time.deltaTime;
+
+                }
+                else if (isFalling)
+                {
+                    if (transform.position.y > tempPos.y)
+                    {
+                        moveDirection.y += (Physics.gravity.y * currentGravScale);
+                        transform.position += moveDirection * Time.deltaTime;
+                    }
+                    else isFalling = false;
+                }
+                else// when not jumping
+                {
+                    isGrounded = true;
+
+                    moveDirection *= Time.deltaTime;
+                    transform.position = new Vector3(transform.position.x + moveDirection.x, tempPos.y, transform.position.z + moveDirection.z);
+                }
+
                 //transform.position += moveDirection * Time.deltaTime;
-                transform.position = new Vector3(transform.position.x + moveDirection.x, tempPos.y, transform.position.z + moveDirection.z) ;
             }
             else transform.position = tempPos;
         }
@@ -88,5 +142,16 @@ public class FloatingVehicle : Interactable
 
 
         beingDriven = false;
+    }
+
+    public IEnumerator Jump(Vector3 floatDistance)
+    {
+        yield return new WaitUntil(()=> transform.position.y >= floatDistance.y + jumpHeight);
+
+        isJumping = true;
+
+        yield return new WaitUntil(()=> transform.position.y <= floatDistance.y);
+
+        isJumping = false;
     }
 }
